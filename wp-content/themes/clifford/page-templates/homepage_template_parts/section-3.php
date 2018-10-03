17b829ddef3a9b04b8cbd8d5e966f4c11a2e3ccb
Garrett Cullen <garrettwcullen@gmail.com>
2018-10-03 10:54:21 -0700

    push

---
+++ wp-content/themes/clifford/page-templates/homepage_template_parts/section-3.php
@@ -0,0 +1,65 @@
+<section id="section_three">
+	
+	<div class="sec_three_inner">
+		
+		<div class="sec_three_left">
+			
+			<h1>Chicago Personal Injury Attorneys</h1>
+			
+			<div class="accolades">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/aw-01.png"/>
+				
+				<div class="accolades_content">
+					
+					<span class="teeup">If you need a lawyer, you have likely found yourself wondering: </span><!-- teeup -->
+					
+					<span class="question">"How do I choose the right one?"</span><!-- question -->
+					
+					<span class="answer">The answer — choose Clifford Law Offices.</span><!-- answer -->
+					
+				</div><!-- accolades_content -->
+				
+			</div><!-- accolades -->
+			
+			<div class="sec_three_content">
+			
+			<p>Our firm is routinely ranked as one of the top Chicago law firms, and we have been included on U.S. News & World Report's list of Best Law Firms. Our attorneys have the experience to protect your interests and help you seek fair compensation for your injuries or the loss of a loved one. </p>
+			
+		</div><!-- sec_three_content -->
+			
+		</div><!-- sec_three_left -->
+		
+		<div class="sec_three_right">
+			
+			<div class="sec_three_image">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/video-thumbnail.jpg"/>
+				
+				<div class="video_wrapper">
+					
+					<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					<div class="video_inner">
+						
+						<span class="line"></span><!-- line -->
+						
+						<div class="video_box">
+							
+							<img src="<?php bloginfo('template_directory');?>/images/logo-mark.svg"/>
+							
+							<span>Watch our video to learn more about us in 90 seconds</span>
+							
+						</div><!-- video_box -->
+						
+					</div><!-- video_inner -->
+					
+				</div><!-- video_wrapper -->
+				
+			</div><!-- sec_three_image -->
+			
+		</div><!-- sec_three_right -->
+
+	</div><!-- sec_three_inner -->
+	
+</section><!-- section_three -->
