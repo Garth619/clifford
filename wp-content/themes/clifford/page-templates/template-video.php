3197823894c0950ca018e21dd4ebdf3a8fd9a78a
Garrett Cullen <garrettwcullen@gmail.com>
2018-10-15 15:14:23 -0700

    push

--- wp-content/themes/clifford/page-templates/template-video.php
+++ wp-content/themes/clifford/page-templates/template-video.php
@@ -4,6 +4,192 @@
 
 get_header(); ?>
 
-
+<div id="internal_main" class="one_col">
+	
+	<h1 class="one_col_title"><?php the_title();?></h1><!-- one_col_title -->
+	
+	<span class="title_line"></span><!-- title_line -->
+	
+	<div class="video_center_wrapper">
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+		<div class="single_video">
+			
+			<a href="//www.youtube-nocookie.com/embed/nfFKJxtlXoM?rel=0&amp;showinfo=0;autoplay=1" data-lity >
+				
+				<div class="thumbnail">
+				
+					<img alt="youtube-nfFKJxtlXoM" src="https://img.youtube.com/vi/nfFKJxtlXoM/mqdefault.jpg"/>
+				
+					<div class="vc_overlay">
+					
+						<?php echo file_get_contents("wp-content/themes/clifford/images/play-button.svg"); ?>
+					
+					</div><!-- vc_overlay -->
+				
+				</div><!-- thumbnail -->
+				
+				<span class="vc_title">Curabitur m quis Curabitur matt odio quis urabitur mattis</span><!-- vc_title -->
+				
+			</a>
+			
+		</div><!-- single_video -->
+		
+	</div><!-- video_center_wrapper -->
+		
+</div><!-- internal_main -->
 
 <?php get_footer(); ?>
