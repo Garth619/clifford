b1fb07e9e3cd06afd25b933f71ae815026db0b08
Garrett Cullen <garrettwcullen@gmail.com>
2018-10-16 14:03:09 -0700

    push

--- wp-content/themes/clifford/page-templates/template-testimonials.php
+++ wp-content/themes/clifford/page-templates/template-testimonials.php
@@ -4,7 +4,136 @@
 
 get_header(); ?>
 
-
-
+<div id="internal_main" class="one_col">
+	
+	<h1 class="one_col_title"><?php the_title();?></h1><!-- one_col_title -->
+	
+	<span class="title_line"></span><!-- title_line -->
+	
+	<div class="testimonial_wrapper">
+		
+		<div class="single_test_wrapper">
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/quote.jpg"/>
+					
+						<span class="test_quote">Nunc eu ante eget ligula molestie elementum ac leo tellus sagittis eget, mauris convallis eleifend.</span><!-- test_quote -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_left -->
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+					
+					<?php echo file_get_contents("wp-content/themes/clifford/images/icon-stars.svg"); ?>
+					
+					<span class="test_description">“Mauris condimentum rutrum velit, vitae laoreet tellus sagittis eget. Duis vitae enim odio. Aliquam pellentesque finibus purus at ullamcorper. Nunc pellentesque dapibus ultricies. Integer in vulputate mi, quis ultricies risus. Sed tincidunt ex nunc, a imperdiet quam congue vitae. Praesent luctus, felis quis vehicula commodo, diam orci hendrerit nunc, sit amet pretium ex magna vitae ante.”</span><!-- test_description -->
+					
+					<span class="test_name">J.D.&nbsp;&nbsp;|&nbsp;&nbsp;molestie vestibulum</span><!-- test_name -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_right -->
+			
+		</div><!-- single_test_wrapper -->
+		
+		<div class="single_test_wrapper">
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/quote.jpg"/>
+					
+						<span class="test_quote">Nunc eu ante eget ligula molestie elementum ac leo tellus sagittis eget, mauris convallis.</span><!-- test_quote -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_left -->
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+					
+					<?php echo file_get_contents("wp-content/themes/clifford/images/icon-stars.svg"); ?>
+					
+					<span class="test_description">“Mauris condimentum rutrum velit, vitae laoreet tellus sagittis eget. Duis vitae enim odio. Aliquam pellentesque finibus purus at ullamcorper. Nunc pellentesque dapibus ultricies. Integer in vulputate mi, quis ultricies risus. Sed tincidunt ex nunc, a imperdiet quam congue vitae. Praesent luctus, felis quis vehicula commodo, diam orci hendrerit nunc, sit amet pretium ex magna vitae ante.”</span><!-- test_description -->
+					
+					<span class="test_name">J.D.&nbsp;&nbsp;|&nbsp;&nbsp;molestie vestibulum</span><!-- test_name -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_right -->
+			
+		</div><!-- single_test_wrapper -->
+		
+		<div class="single_test_wrapper">
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/quote.jpg"/>
+					
+						<span class="test_quote">Nunc eu ante eget ligula molestie elementum ac leo tellus sagittis eget, mauris convallis.</span><!-- test_quote -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_left -->
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+					
+					<?php echo file_get_contents("wp-content/themes/clifford/images/icon-stars.svg"); ?>
+					
+					<span class="test_description">“Mauris condimentum rutrum velit, vitae laoreet tellus sagittis eget. Duis vitae enim odio. Aliquam pellentesque finibus purus at ullamcorper. Nunc pellentesque dapibus ultricies. Integer in vulputate mi, quis ultricies risus. Sed tincidunt ex nunc, a imperdiet quam congue vitae. Praesent luctus, felis quis vehicula commodo, diam orci hendrerit nunc, sit amet pretium ex magna vitae ante.”</span><!-- test_description -->
+					
+					<span class="test_name">J.D.&nbsp;&nbsp;|&nbsp;&nbsp;molestie vestibulum</span><!-- test_name -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_right -->
+			
+		</div><!-- single_test_wrapper -->
+		
+		<div class="single_test_wrapper">
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/quote.jpg"/>
+					
+						<span class="test_quote">Nunc eu ante eget ligula molestie elementum ac leo tellus sagittis eget, mauris convallis.</span><!-- test_quote -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_left -->
+			
+			<div class="test_col">
+				
+				<div class="test_inner">
+					
+					<?php echo file_get_contents("wp-content/themes/clifford/images/icon-stars.svg"); ?>
+					
+					<span class="test_description">“Mauris condimentum rutrum velit, vitae laoreet tellus sagittis eget. Duis vitae enim odio. Aliquam pellentesque finibus purus at ullamcorper. Nunc pellentesque dapibus ultricies. Integer in vulputate mi, quis ultricies risus. Sed tincidunt ex nunc, a imperdiet quam congue vitae. Praesent luctus, felis quis vehicula commodo, diam orci hendrerit nunc, sit amet pretium ex magna vitae ante.”</span><!-- test_description -->
+					
+					<span class="test_name">J.D.&nbsp;&nbsp;|&nbsp;&nbsp;molestie vestibulum</span><!-- test_name -->
+					
+				</div><!-- test_inner -->
+				
+			</div><!-- test_right -->
+			
+		</div><!-- single_test_wrapper -->
+		
+	</div><!-- testimonial_wrapper -->
+		
+</div><!-- internal_main -->
 
 <?php get_footer(); ?>
