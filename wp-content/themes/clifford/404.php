8e7bd3cc39403948837cf3d816a1b9bdccc7b45f
Garrett Cullen <garrettwcullen@gmail.com>
2018-10-16 10:46:31 -0700

    push

--- wp-content/themes/clifford/404.php
+++ wp-content/themes/clifford/404.php
@@ -1,17 +1,36 @@
 <?php get_header(); ?>
 
-
-<span onclick="goBack()">go back</span>
+<div id="internal_main" class="one_col">
 	
-<!--
-	<script type="text/javascript">
+	<div class="not_found">
+		
+		<h1 class="not_found_header">404</h1>
+		
+		<span class="not_found_small_header">Page Not Found</span><!-- not_found_small_header -->
+		
+		<span class="not_found_description">looks like the page you’re looking for doesn’t exist</span><!-- not_found_description -->
+		
+		<span class="go_back" onclick="goBack()">go back now</span><!-- go_back -->
+		
+	</div><!-- not_found -->
+	
+	
+		
+</div><!-- internal_main -->
+
+
+<script type="text/javascript">
 
 		function goBack() {
     	window.history.back();
 		}
 
-	</script>
--->
-	
+</script>
 
 <?php get_footer(); ?>
+
+
+
+
+
+	
